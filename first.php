6b770adbb86c89d46cf9bf9012393e4931e3647e
Garance "gua <garancevermorel66@gmail.com>
2024-01-30 10:40:27 +0100

    Update first.php

--- first.php
+++ first.php
@@ -1,12 +1,22 @@
 <html>
-<head>
-<title>Test</title>
-</head>
+   <head>
+    <title>Test</title>
+   </head>
 
-<body>
-<p>un bout de code en HTML</p>
-<?php
-echo 'Mon premier script en PHP';
-?>
-</body>
+   <body>
+      <p>un bout de code en HTML</p>
+      <?php
+      echo 'Mon premier script en PHP';
+
+      $fruit = Array();
+      $fruit[] = "fraise";
+      $fruit[] = "banane";
+      $fruit[] = "abricot";
+
+      $fruit = Array();
+      $fruit['le_meilleur'] = "fraise";
+      $fruit['le_prefere_de_Julien'] = "banane";
+      $fruit['mon_prefere'] = "abricot";
+      ?>
+   </body>
 </html>
