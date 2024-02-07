e9bb1fa9d018532b3792728b1d9b72c945015ab9
gua <garancevermorel66@gmail.com>
2024-02-07 11:31:14 +0100

    addition

--- calcul.php
+++ calcul.php
@@ -1,1 +1,15 @@
-function
+<?php
+
+    // Addition de deux nombres entiers par addition successives de 1:
+
+    function addition($a, $b) {
+        $result = $a;
+        for ($i=0; $i < $b; $i++) { 
+            $result++;
+        }
+        return $result;
+    }
+
+    
+  
+?>
